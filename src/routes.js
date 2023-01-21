ea8ade91063adf6a4c125e81551d82fd55341f5b
Anwar Fuadi <anwarfuadi07@outlook.com>
2023-01-21 07:27:24 +0700

    Commit 6: Menambah isi kode getBookByIdHandler

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 const addBookHandler = require('./handler/add_book_handler')
 const getAllBooksHandler = require('./handler/get_all_books_handler')
+const getBookByIdHandler = require('./handler/get_book_by_id_handler')
 
 const routes = [
   {
@@ -11,6 +12,11 @@ const routes = [
     method: 'GET',
     path: '/books',
     handler: getAllBooksHandler
+  },
+  {
+    method: 'GET',
+    path: '/books/{id}',
+    handler: getBookByIdHandler
   }
 ]
 
